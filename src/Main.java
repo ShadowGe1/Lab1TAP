4072b4020c3b262cc2899f82dd0d694e25bd8320
ShadowGe1 <122978262+ShadowGe1@users.noreply.github.com>
2024-02-09 10:19:20 +0200

    Code optimization

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,12 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        String Text, Subsir;
         Scanner sc = new Scanner(System.in);
         System.out.println("Introduceti textul: ");
-        Text = sc.nextLine();
-        System.out.println("Introduceti textul care doriti sa fie adaugat: ");
-        Subsir = sc.nextLine();
-        TextRedactor redactor = new TextRedactor(Text, Subsir);
+        String Text = sc.nextLine();
+        System.out.println("Introduceti textul pentru inlocuire: ");
+        String text = sc.nextLine();
+        TextRedactor redactor = new TextRedactor(Text, text);
         redactor.redactare();
         sc.close();
     }
--- src/TextRedactor.java
+++ src/TextRedactor.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class TextRedactor {
     private String TEXT;
-    private final String SUBSIR;
+    private  final String SUBSIR;
     public TextRedactor(String TEXT, String SUBSIR){
         this.TEXT = TEXT;
         this.SUBSIR = SUBSIR;
@@ -11,18 +11,19 @@ public class TextRedactor {
     public void redactare(){
         char caracter;
         int length = TEXT.length();
+        System.out.println("Introduceti caracterul dupa care se va introduce subsirul introdus: ");
         Scanner sd = new Scanner(System.in);
         do {
-            System.out.println("Introduceti caracterul dupa care se va introduce subsirul introdus: ");
             caracter = sd.next().charAt(0);
             for(int i = 0; i < TEXT.length(); i++){
                 if(caracter == TEXT.charAt(i)){
                     TEXT = TEXT.substring(0, i + 1) + SUBSIR + TEXT.substring(i + 1);
+                    i += SUBSIR.length();
                 }
             }
             if(length == TEXT.length()){
                 System.out.println("Caracterul introdus nu a fost gasit!!");
-                continue;
+                return;
             }
             System.out.println(TEXT);
         }while(length == TEXT.length());
