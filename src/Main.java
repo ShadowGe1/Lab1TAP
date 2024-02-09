0334c5da0403b74fe72108b540b673fb6773edea
Aesthetic <andreiclima5@gmail.com>
2024-02-09 10:24:44 +0200

    lalal

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
         System.out.println("Introduceti textul: ");
         String Text = sc.nextLine();
